b3aca64f2290c10d83b8e66efc3b1b8353aaf3f0
Timothy Rascher <timrascher@gmail.com>
2021-04-28 10:42:48 -0700

    Added in ability for URLs of directories to quickly create themselves.

--- Sources/QuickCommon/URL/URL+Exsistance.swift
+++ Sources/QuickCommon/URL/URL+Exsistance.swift
@@ -11,4 +11,13 @@ public extension URL {
     func exsists(in fileManager: FileManager) -> FileManager.Exsistance {
         fileManager.exsists(url: self)
     }
+    func createDirectoryIfItDoesntExsist(_ manager: FileManager? = nil) throws {
+        let manager = manager ?? FileManager.default
+        let exsists = self.exsists
+        if exsists == .isDirectory { return }
+        if exsists == .isFile {
+            try manager.removeItem(at: self)
+        }
+        try manager.createDirectory(at: self, withIntermediateDirectories: true, attributes: nil)
+    }
 }
